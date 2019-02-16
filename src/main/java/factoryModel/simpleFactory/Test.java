3a70d94e3e08077db30f447e65ea64a8fb3c5f4e
lusq <lushuqin@avicare.cn>
2019-02-16 10:14:31 +0800

    @Title 新增代理模式

--- src/main/java/factoryModel/simpleFactory/Test.java
+++ src/main/java/factoryModel/simpleFactory/Test.java
@@ -3,7 +3,7 @@ package factoryModel.simpleFactory;
 public class Test {
     public static void main(String[] args) {
         BlackHuman blackHuman = HumanFactory.createHuman(BlackHuman.class);
-        blackHuman.getColor();;
+        blackHuman.getColor();
         blackHuman.talk();
         WhiteHuman whiteHuman = HumanFactory.createHuman(WhiteHuman.class);
         whiteHuman.getColor();
